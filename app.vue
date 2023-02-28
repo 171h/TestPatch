df37513f046450c700d6c6f3620684be72ad9074
HWJ Zbook <1091088708@qq.com>
2023-02-28 18:13:43 +0800

    add img

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <button @click="upd">Test Patch</button>
+    <div style="font-size: 25px;">
+      when i click "Test Patch" button, the console will show:"400 Bad request"，but the api is work well in ApiPost.
+      <hr>
+      i think the problem is the body of the request, but i don't know how to fix it.
+    </div>
+    <div style="margin-top: 2rem;">
+      <div style="color: red; background-color: black;">400 Bad request</div>
+      <img src="~/assets/Snipaste_2023-02-28_18-00-16.png" alt="">
+    </div>
+    <hr>
+    <div>
+      <div style="color: red; background-color: black;">work well in ApiPost</div>
+      <img src="~/assets/Snipaste_2023-02-28_18-06-08.png" alt="">
+    </div>
   </div>
 </template>
 
